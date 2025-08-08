2eed41a5cbfe627c123d475f7154af14a7ebe093
Chiagoziem Sunday <gozmansunday@gmail.com>
2025-08-08 01:36:08 +0100

    update the client homepage

--- apps/client/src/routes/index.tsx
+++ apps/client/src/routes/index.tsx
@@ -1,10 +1,8 @@
 import { createFileRoute } from "@tanstack/react-router";
 
-import env from "@/lib/env";
-
 export const Route = createFileRoute("/")({
   beforeLoad: async () => {
-    console.log(env.SECRET);
+    console.log(process.env.SECRET);
   },
   loader: async () => {
     return { secret: process.env.SECRET, apiUrl: process.env.API_URL };
@@ -16,17 +14,20 @@ function Homepage() {
   const { secret } = Route.useLoaderData();
 
   return (
-    <div className="container flex min-h-dvh flex-col items-center justify-center gap-3 p-4">
-      <h1 className="font-bold font-jetbrains text-4xl">
-        Welcome to the Homepage
+    <div className="container flex min-h-dvh flex-col items-center justify-center gap-3 p-4 text-center">
+      <h1 className="font-bold font-jetbrains text-2xl md:text-3xl lg:text-4xl">
+        Welcome to the CF STACK!!!
       </h1>
-      <p className="font-inter text-lg">
+      <p className="font-inter text-base md:text-lg">
         This is the main entry point of our application.
       </p>
-      <p className="font-inter text-lg">SECRET: {secret}</p>
-      <p className="font-inter text-lg">
-        API URL: {import.meta.env.VITE_API_URL}
-      </p>
+
+      <div>
+        <p className="font-roboto text-base md:text-lg">SECRET: {secret}</p>
+        <p className="font-roboto text-base md:text-lg">
+          API URL: {import.meta.env.VITE_API_URL}
+        </p>
+      </div>
     </div>
   );
 }
